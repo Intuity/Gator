bd4c966398e68ed7603f20353f203c4ea0a647f4
Peter Birch <peter@lightlogic.co.uk>
2022-11-19 15:44:06 +0000

    dummy.sh: Using some more logging features

--- dummy.sh
+++ dummy.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+# Arguments
 tgt=${1:-10}
 
+# Use directive logging interactive
+python -m gator.logger --severity DEBUG "This is a debug message"
+python -m gator.logger --severity INFO "This is an info message"
+python -m gator.logger --severity WARNING "This is a warning message"
+python -m gator.logger --severity ERROR "This is an error message"
+
+# Use STDOUT and STDERR
+echo "This is STDOUT"
+>&2 echo "This is STDERR"
+
+# Launch a bunch of subprocesses
 do_something () {
     echo "Launching $1"
     sleep 5
@@ -15,6 +27,8 @@ done
 
 wait
 
+# Run a high intensity process
+echo "Running high intensity calculation"
 a=0
 b=1234
 for i in $(seq 1000000); do
