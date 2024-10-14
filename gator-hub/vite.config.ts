cfabc61e698ed0e991ce0c4a732992e3bdd7a5eb
Edward Kotarski <edktrsk@gmail.com>
2024-10-14 08:38:44 +0100

    Copy from bucket

--- gator-hub/vite.config.ts
+++ gator-hub/vite.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import * as path from 'path'
+/*
+ * SPDX-License-Identifier: MIT
+ * Copyright (c) 2023-2024 Vypercore. All Rights Reserved
+ */
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+// Plugin that synchronises vite's path searching with tsconfig settings.
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    root: path.resolve(__dirname, "src"),
-    resolve: {
-        alias: {
-            "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap")
-        }
-    },
-    plugins: [react()]
-})
+    plugins: [react(), tsconfigPaths()],
+});
